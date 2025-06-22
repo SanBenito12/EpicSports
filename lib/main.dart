496fc4902e4cf802e0df3b62a094bf11d3a298b3
sedc.castilloz <sedc.castilloz@gmail.com>
2025-06-22 03:01:06 +0000

    Primera subida del proyecto Flutter con Firebase

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('✅ Firebase inicializado correctamente');
+  } catch (e) {
+    print('❌ Error al inicializar Firebase: $e');
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sports Stats App',
+      theme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.pink),
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
